5c7fc90aa3ab1229166a730303ed04160c8cd1ef
PHAM Tuan Anh <phamtuananh3031998@gmail.com>
2024-03-26 18:05:40 +0100

    renommer une classe

--- jhotdraw/jhotdraw-gui/src/main/java/org/jhotdraw/gui/plaf/palette/PaletteRootPaneUINew.java
+++ jhotdraw/jhotdraw-gui/src/main/java/org/jhotdraw/gui/plaf/palette/PaletteRootPaneUINew.java
@@ -1,5 +1,5 @@
 /**
- * @(#)PaletteRootPaneUI.java
+ * @(#)PaletteRootPaneUINew.java
  *
  * <p>Copyright (c) 2008 The authors and contributors of JHotDraw. You may not use, copy or modify
  * this file, except in compliance with the accompanying license terms.
@@ -12,10 +12,10 @@ import javax.swing.*;
 import javax.swing.plaf.*;
 import javax.swing.plaf.basic.BasicRootPaneUI;
 
-/** PaletteRootPaneUI. */
-public class PaletteRootPaneUI extends BasicRootPaneUI {
+/** PaletteRootPaneUINew. */
+public class PaletteRootPaneUINew extends BasicRootPaneUI {
 
-  private static RootPaneUI rootPaneUI = new PaletteRootPaneUI();
+  private static RootPaneUI rootPaneUI = new PaletteRootPaneUINew();
 
   public static ComponentUI createUI(JComponent c) {
     return rootPaneUI;
